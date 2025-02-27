e4e4ed24450dab39e2e48ebe61fe319716597643
ZainAhmed67 <zainhmed5102@gmail.com>
2025-02-27 07:08:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.querySelector('.navbar');
+
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
+
